934d43a8467c90a37c0671a5e76bed3c3b1b3c8c
rliston <rliston@uvm.edu>
2021-10-25 15:47:38 -0400

    Implemented the deck class

---
+++ backend/deck.py
@@ -0,0 +1,38 @@
+# deck class
+
+import random
+
+# list containing the suits
+suitsList = ["Spades", "Clubs", "Diamonds", "Hearts"]
+
+class Deck:
+
+    # initializer
+    def __init__(self):
+        # empty array to hold cards
+        self.cards = []
+        self.buildDeck()
+
+    # create a deck of cards using Card class
+    def buildDeck(self):
+        # iterate through suits and then values 1 - 14
+        for s in suitsList:
+            for i in range(2, 14):
+                # anything 10 or higher is automatically a 10
+                if i <= 9:
+                    self.cards.append(Card(s, i))
+                else:
+                    self.cards.append(Card(s, 10))
+
+    # randomly deal a single card
+    def deal(self):
+        # get a random card using a random index
+        randIndex = random.randrange(len(self.cards))
+        dealtCard = self.cards[randIndex]
+
+        # remove that card from the deck
+        self.cards.remove(dealtCard)
+
+        # return (deal) the randomly selected card
+        return dealtCard
+
