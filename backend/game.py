a00f0c6dc753da8d58d49103e5fa5481ae4e5b98
claykaufmann <claykaufmann@gmail.com>
2021-10-27 11:59:15 -0400

    update game class

--- backend/game.py
+++ backend/game.py
@@ -8,8 +8,9 @@ Game class represents a game of blackjack
 class Game():
     def __init__(self, id) -> None:
         self.id = id
-        self.player = Player()  # TODO have player classes instantiate correctly
-        self.dealer = Player()
+        # TODO have player classes instantiate correctly
+        self.player = Player(dealer=False)
+        self.dealer = Player(dealer=True)
         self.deck = Deck()
 
     def initial_deal(self):
@@ -20,16 +21,20 @@ class Game():
         dealer_card2 = self.deck.deal()
 
         # TODO update this to correctly add cards when methods are implemented
-        self.player.cards = [player_card1, player_card2]
-        self.dealer.cards = [dealer_card1, dealer_card2]
+        self.player.add_card(player_card1)
+        self.player.add_card(player_card2)
+
+        self.dealer.add_card(player_card1)
+        self.dealer.add_card(player_card2)
 
     # TODO impement this method...
     def action_input(self, action):
         if action == "hit":
             new_card = self.deck.deal()
-            self.player.cards.append(new_card)
+            self.player.add_card(new_card)
 
         # check if player went over 21...
+        self.dealer.game_action()
 
         # check if dealer is over 21...
 
