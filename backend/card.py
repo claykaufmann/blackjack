c668807dd60c85b3ba3152500e4686ab6be0cefc
Vincent Lai <vincent.lai@uvm.edu>
2021-10-29 12:00:37 -0400

    json card

--- backend/card.py
+++ backend/card.py
@@ -6,3 +6,9 @@ class Card:
     
     def to_string(self):
         return self.value + ' of ' + self.suit
+
+    def json(self):
+        return{
+            "value": self.value,
+            "suit": self.suit
+        }
