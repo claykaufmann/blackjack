a2464b1f0798f34a72c918b57c9b79ece6baf640
claykaufmann <claykaufmann@gmail.com>
2021-11-29 13:38:42 -0500

    card suits appearing correctly

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -6,28 +6,28 @@ const Card = (props) => {
 
   let card = <></>;
   /* switch case for suits */
-  // let cSuit = '';
+  let cSuit = '';
 
-  // switch(suit) {
-  //   case 'Hearts':
-  //     cSuit = <>&hearts</>;
-  //     break;
+  switch (suit) {
+    case 'Hearts':
+      cSuit = <>&hearts;</>;
+      break;
 
-  //   case 'Diamonds':
-  //     cSuit = <>&diams</>;
-  //     break;
+    case 'Diamonds':
+      cSuit = <>&diams;</>;
+      break;
 
-  //   case 'Spades':
-  //     cSuit = <>&spades</>;
-  //     break;
+    case 'Spades':
+      cSuit = <>&spades;</>;
+      break;
 
-  //   case 'Clubs':
-  //     cSuit = <>&clubs</>;
-  //     break;
+    case 'Clubs':
+      cSuit = <>&clubs;</>;
+      break;
 
-  //   default:
-  //     break;
-  // }
+    default:
+      break;
+  }
 
   if (visibility === false) {
     card = (
@@ -40,7 +40,7 @@ const Card = (props) => {
       <>
         <div className="Card-front">
           <div className="Top-value"> {value} </div>
-          <div className="Suit"> {suit} </div>
+          <div className="Suit"> {cSuit} </div>
           <div className="Bottom-value"> {value} </div>
         </div>
       </>
